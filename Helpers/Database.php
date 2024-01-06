cc930b0400fbe4bf883c39ebf52a65792788f865
Fikret CURE <fikretcure@gmail.com>
2024-01-06 17:54:23 +0300

    feat : Updated Database.php

--- Helpers/Database.php
+++ Helpers/Database.php
@@ -5,7 +5,7 @@ try {
     /**
      * @return PDO
      */
-    function OpenCon()
+    function OpenCon(): PDO
     {
         $connection = new PDO("mysql:host=mysql;dbname=" . Config::MYSQL_DB_NAME, Config::MYSQL_USER_NAME, Config::MYSQL_PASSWORD);
         $connection->exec("SET NAMES utf8");
