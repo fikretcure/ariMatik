e09f43b77a247822198bf4e820990835c441d3d7
Fikret CURE <fikretcure@gmail.com>
2024-01-06 17:52:40 +0300

    feat : Updated Database.php

--- Helpers/Database.php
+++ Helpers/Database.php
@@ -1,17 +1,17 @@
 <?php
+try {
+    require_once 'Config.php';
 
-require_once './Helpers/Config.php';
-
-/**
- * @return PDO|void
- */
-function OpenCon()
-{
-    try {
+    /**
+     * @return PDO
+     */
+    function OpenCon()
+    {
         $connection = new PDO("mysql:host=mysql;dbname=" . Config::MYSQL_DB_NAME, Config::MYSQL_USER_NAME, Config::MYSQL_PASSWORD);
         $connection->exec("SET NAMES utf8");
         return $connection;
-    } catch (PDOException $e) {
-        echo($e->getMessage());
     }
+
+} catch (Throwable $e) {
+    echo($e);
 }
